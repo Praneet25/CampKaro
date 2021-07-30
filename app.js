deb14e17ec87b6a32a75f46a5fea4ca222cd888d
Praneet25 <praneetkshd180@gmail.com>
2021-07-30 15:09:19 +0530

    add start script

--- app.js
+++ app.js
@@ -128,7 +128,7 @@ app.use((err, req, res, next) => {
         err
     });
 })
-
-app.listen(3000, () => {
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
     console.log('listening on 3000');
 });
